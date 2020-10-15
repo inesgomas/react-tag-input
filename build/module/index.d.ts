f642fd9cff0d52d46cf02596dd1bad3e55b3f7cf
Inês Gomes <igomes@codacy.com>
2020-10-15 16:29:50 +0100

    clean: Remove build folder from git ignore

---
+++ build/module/index.d.ts
@@ -0,0 +1,30 @@
+import React from "react";
+declare type Tags = string[];
+export interface ReactTagInputProps {
+    tags: Tags;
+    onChange: (tags: Tags) => void;
+    placeholder?: string;
+    maxTags?: number;
+    validator?: (val: string) => boolean;
+    editable?: boolean;
+    readOnly?: boolean;
+    removeOnBackspace?: boolean;
+    delimiters?: number[];
+}
+interface State {
+    input: string;
+}
+export default class ReactTagInput extends React.Component<ReactTagInputProps, State> {
+    state: {
+        input: string;
+    };
+    inputRef: React.RefObject<HTMLInputElement>;
+    onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onInputKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+    onPaste: (e: React.ClipboardEvent<HTMLInputElement>) => void;
+    addTag: (value: string) => void;
+    removeTag: (i: number) => void;
+    updateTag: (i: number, value: string) => void;
+    render(): JSX.Element;
+}
+export {};
